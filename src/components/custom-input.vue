cd8d13488c931f640d79386aedea85921cc0cff7
Bardiya Fatemi <38523805+LostPXL@users.noreply.github.com>
2022-02-28 12:16:09 +0330

    Added custom input component not in working order

---
+++ src/components/custom-input.vue
@@ -0,0 +1,30 @@
+<template>
+<form class="form" @submit.prevent="handleSubmit">
+    <label>
+        <input type="text">
+        <label>
+
+</form>
+</template>
+
+<script>
+export default {
+    name: "custom-input",
+    props: {
+        
+        }
+    ,methods: {
+        handleSubmit() {
+            console.log('Submitted')
+            }
+    }
+    
+</script>
+<style scoped lang="scss">
+.form {
+    margin: auto;
+    height: 100px;
+    width: 100px;
+    box-shadow: 15px 15px red;
+}
+</style>
